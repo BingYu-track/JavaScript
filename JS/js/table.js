e456c5011d33396392dcd0acd718ef0c3e0ba213
huxiaowei <525782303@qq.com>
2017-12-12 19:30:09 +0800

    111

--- JS/js/table.js
+++ JS/js/table.js
@@ -2,6 +2,9 @@
  * 
  * @param {Object} propetyNames
  */
-var TableToArray = function(propetyNames/*自定义属性名数组(属性的顺序要与表格的顺序一致)*/,table/*table元素*/,index/*指定要转成时间戳订单属性所在数组的索引(是个数组)*/){ //返回表格数组，且表格中的数据与
-	
+var TableToArray = function(propetyNames/*自定义属性名数组(属性的顺序要与表格的顺序一致)*/,table/*table元素*/,indexs/*指定要转成时间戳订单属性所在数组的索引(是个数组)*/){ //返回表格数组，且表格中的数据与
+	var table = {};//创建一个数组
+	propetyNames.forEach(function(a,b,c){
+		table[a];
+	});
 }
